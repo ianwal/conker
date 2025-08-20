9b69dd5039e0cc909a2ba8126d9dd8db29d82d21
ianwal <git.overarch017@passinbox.com>
2025-08-20 19:03:11 +0000

    Match func_1503FB08

--- conker/src/game_6CEA0.c
+++ conker/src/game_6CEA0.c
@@ -5,6 +5,18 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6CEA0/func_1503F9F0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_6CEA0/func_1503FB08.s")
+extern s32 D_80084488;
+extern s32 D_8008448C;
+extern s8 D_80084490;
+extern s8 D_80084494;
+extern f32 D_800C6850;
+
+void func_1503FB08(void) {
+    D_80084488 = 0;
+    D_800C6850 = 0.0f;
+    D_8008448C = 0;
+    D_80084490 = 0x1E;
+    D_80084494 = 0;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/game_6CEA0/func_1503FB40.s")
