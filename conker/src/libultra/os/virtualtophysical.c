f9fcfadfc171fb4c4b8e2166b7c1d81feddae784
ianwal <git.overarch017@passinbox.com>
2025-08-21 19:15:43 +0000

    Match virtualtophysical,recvmesg, sendmesg

---
+++ conker/src/libultra/os/virtualtophysical.c
@@ -0,0 +1,15 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+#include "R4300.h"
+
+
+u32 osVirtualToPhysical(void* addr) {
+    if (IS_KSEG0(addr)) {
+        return K0_TO_PHYS(addr);
+    } else if (IS_KSEG1(addr)) {
+        return K1_TO_PHYS(addr);
+    } else {
+        return __osProbeTLB(addr);
+    }
+}
