6800f78f03c97886f640e10cb9aa4b098970931f
ianwal <git.overarch017@passinbox.com>
2025-08-21 19:11:49 +0000

    Match stopthread

---
+++ conker/src/libultra/os/stopthread.c
@@ -0,0 +1,25 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+void osStopThread(OSThread* t) {
+    register u32 saveMask = __osDisableInt();
+    register u16 state;
+
+    state = (t == NULL) ? OS_STATE_RUNNING: t->state;
+
+    switch (state) {
+        case OS_STATE_RUNNING:
+            __osRunningThread->state = OS_STATE_STOPPED;
+            __osEnqueueAndYield(NULL);
+            break;
+        case OS_STATE_RUNNABLE:
+        case OS_STATE_WAITING:
+            t->state = OS_STATE_STOPPED;
+            __osDequeueThread(t->queue, t);
+            break;
+    }
+
+    __osRestoreInt(saveMask);
+}
