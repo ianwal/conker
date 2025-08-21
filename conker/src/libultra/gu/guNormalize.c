9abd7011b0f99b6f948da663a505ca2e96ab4f85
ianwal <git.overarch017@passinbox.com>
2025-08-21 05:17:01 +0000

    Partially match guNormalize

--- conker/src/libultra/gu/guNormalize.c
+++ conker/src/libultra/gu/guNormalize.c
@@ -1,6 +1,14 @@
-#include <ultra64.h>
-#include "functions.h"
-#include "variables.h"
-
-
+// TODO: This matches perfectly, but doesn't match in the game.
+// https://decomp.me/scratch/eXEp3
+// See libultra https://github.com/decompals/ultralib/blob/48f9f9084e06036f1a613187186c8b0f5b68ce40/src/gu/normalize.c
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/gu/guNormalize/guNormalize.s")
+
+// void guNormalize(float *x, float *y, float *z)
+// {
+//     float	m;
+// 
+//     m = 1/sqrtf((*x)*(*x) + (*y)*(*y) + (*z)*(*z));
+//     *x *= m;
+//     *y *= m;
+//     *z *= m;
+// }
