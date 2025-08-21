9506050332c797ee6120b049e92a85da2690de4d
ianwal <git.overarch017@passinbox.com>
2025-08-21 18:40:41 +0000

    Match pirawread

--- conker/src/libultra/io/pirawread.c
+++ conker/src/libultra/io/pirawread.c
@@ -2,11 +2,22 @@
 #include "functions.h"
 #include "variables.h"
 #include "piint.h"
+#include "assert.h"
 
-s32 osPiRawReadIo(u32 devAddr, u32 *data)
-{
+s32 __osPiRawReadIo(u32 devAddr, u32* data) {
     register u32 stat;
+
+#ifdef _DEBUG
+    if (devAddr & 0x3) {
+        __osError(ERR_OSPIRAWREADIO, 1, devAddr);
+        return -1;
+    }
+#endif
+
+    // assert(data != NULL); // TODO: Fix assert()
+
     WAIT_ON_IOBUSY(stat);
     *data = IO_READ((u32)osRomBase | devAddr);
+
     return 0;
 }
