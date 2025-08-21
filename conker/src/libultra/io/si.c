16c7a7e62007d40f947af1b2c582ba82ce04be30
ianwal <git.overarch017@passinbox.com>
2025-08-21 20:55:42 +0000

    Match si

---
+++ conker/src/libultra/io/si.c
@@ -0,0 +1,14 @@
+#include <ultra64.h>
+#include "functions.h"
+#include "variables.h"
+
+
+int __osSiDeviceBusy(void) {
+    register u32 stat = IO_READ(SI_STATUS_REG);
+
+    if (stat & (SI_STATUS_DMA_BUSY | SI_STATUS_RD_BUSY)) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
